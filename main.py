21f47d6e5a89cbc53e2db3a693b99315eecdc2fd
belzabeli <belzabeliani@gmail.com>
2022-07-15 13:02:55 +0400

    final version

---
+++ main.py
@@ -0,0 +1,19 @@
+def bigger_is_greater(w):
+    arr = list(w)
+    i = len(arr) - 1
+    while i > 0 and arr[i - 1] >= arr[i]:
+        i -= 1
+    if i <= 0:
+        return "no answer"
+
+    j = len(arr) - 1
+    while arr[j] <= arr[i - 1]:
+        j -= 1
+    arr[i - 1], arr[j] = arr[j], arr[i - 1]
+
+    arr[i:] = arr[len(arr) - 1: i - 1: -1]
+    return "".join(arr)
+
+
+for _ in range(int(input())):
+    print(bigger_is_greater(input()))
